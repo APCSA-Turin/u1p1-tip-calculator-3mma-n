43d83d5ef658621a4b7af94a1ec69c95c00c3e6c
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-09-25 16:25:32 +0000

    final w/ feedback

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -4,23 +4,26 @@ public class TipCalculator {
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
+
         // calculates each value to be printed
-        double tip = cost * (percent / 100.0);
-        double fullCost = cost + tip; 
-        double costPer = cost / people;
+        double tip = cost * (percent / 100.0); // divides percent by 100 so it can be used as a decimal, then multiplies by cost
+        double fullCost = cost + tip;  // calculates the cost including tip
+        double costPer = cost / people; // each of the next 3 divide previous results by the # of people to split the cost
         double tipPer = tip / people;
         double fullCostPer = fullCost / people;
 
         // rounds all values to 2 decimal places (after all the math so the math is still accurate)
+        // I found an explanation for Math.round here: https://www.educative.io/answers/how-to-use-the-java-mathround-method
         tip = Math.round(tip * 100) / 100.0;
         fullCost = Math.round(fullCost * 100) / 100.0;
         costPer = Math.round(costPer * 100) / 100.0;
         tipPer = Math.round(tipPer * 100) / 100.0;
         fullCostPer = Math.round(fullCostPer * 100) / 100.0;
 
+        // adds all values (concatenated to their respective strings) to the result
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: $" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: " + percent + "%\n");
+        result.append("Total bill before tip: $" + cost + "\n"); // dollar signs are added to all strings except percentage
+        result.append("Total percentage: " + percent + "%\n"); // percent symbol is added after the percent value
         result.append("Total tip: $" + tip + "\n");
         result.append("Total Bill with tip: $" + fullCost + "\n");
         result.append("Per person cost before tip: $" + costPer + "\n");
@@ -40,4 +43,3 @@ public class TipCalculator {
         System.out.println(calculateTip(people,percent,cost));
     }
 }
-        
